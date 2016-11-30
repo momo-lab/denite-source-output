ccc90102885cc193f5b1eb83ce5f6c57f2c1a36c
momotaro <momotaro.n@gmail.com>
2016-11-30 14:11:22 +0900

    first commit

---
+++ rplugin/python3/denite/source/output.py
@@ -0,0 +1,31 @@
+# FILE: output.py
+# AUTHOR: momotaro <momotaro.n@gmail.com>
+# License: MIT license
+
+from .base import Base
+import os
+import re
+
+class Source(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'output'
+        self.kind = 'word'
+        self.syntax_name = 'deniteSource_output'
+
+    def on_init(self, context):
+        command = ' '.join(context['args'])
+        context['__command'] = command
+
+    def gather_candidates(self, context):
+        message = self.vim.call('denite#util#redir', context['__command'])
+        message = re.sub('^(\r\n|\n)', '', message)
+        return list(map(lambda x: { 'word': x, 'action__text': x },
+            re.split('\r\n|\n', message)))
+
+    def highlight_syntax(self):
+        self.vim.command('syntax include @Vim syntax/vim.vim')
+        self.vim.command('syntax region ' + self.syntax_name + 'Vim'
+                ' start=// end=/$/ contains=@Vim containedin=' + self.syntax_name)
